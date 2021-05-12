faae7ec8a87e5cbf5272ef68cb7138e1691e0ac9
Takayuki Wada <takayuki.wada@hotmail.fr>
2021-05-12 22:31:41 +0200

    linked some pages with signin signup and profile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import {Home, NavBar} from "./components"
+import Profile from "./components/Profile";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 
@@ -11,8 +12,9 @@ function App() {
       <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/signup" component={Signin} />
-        <Route path="/signin" component={Signup} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/profile" component={Profile} />
       </Switch>
     </div>
   );
