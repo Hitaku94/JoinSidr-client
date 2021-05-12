22a918e3ed3d2d7dfa62ee66b2075fca81c28876
Takayuki Wada <takayuki.wada@hotmail.fr>
2021-05-12 16:55:28 +0200

    added a nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import {Home, NavBar} from "./components"
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={Home} />
+        <Route path="/signup" component={Signin} />
+        <Route path="/signin" component={Signup} />
       </Switch>
     </div>
   );
